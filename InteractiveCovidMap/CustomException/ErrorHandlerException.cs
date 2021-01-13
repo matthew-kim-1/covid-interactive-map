6fa087935d9af62ab81a937ef5c034608997f16f
Matthew Kim <minsub95@hotmail.com>
2021-01-13 09:51:36 -0500

    Added global exception handling

---
+++ InteractiveCovidMap/CustomException/ErrorHandlerException.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Security.Authentication;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CovidTracking.CustomException
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            response.StatusCode = exception switch
+            {
+                ValidationException _ => (int)HttpStatusCode.BadRequest,
+                FormatException _ => (int)HttpStatusCode.BadRequest,
+                AuthenticationException _ => (int)HttpStatusCode.Forbidden,
+                NotImplementedException _ => (int)HttpStatusCode.NotImplemented,
+                NotFoundException _ => (int)HttpStatusCode.NotFound,
+                _ => (int)HttpStatusCode.InternalServerError,
+            };
+
+            var result = JsonSerializer.Serialize(new { message = exception?.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
